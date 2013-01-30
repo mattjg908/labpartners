ad4de9716c8601ec64d95e13f954acb980d85650
Ron Jones <rrrron@gmail.com>
2013-01-30 16:31:37 -0500

    added group adjuster code

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 students = ["Stephen Bae", "Ralph Bernardo", "Nick Blanchet", "Ben Clayman", "Tony Contreras", "Alex Fogg", "Shefali Friesen", "Matthew Galvin", "Eric Holland", "Nicky Hughes", "Ron Jones", "Jeffrey Katz", "Ryan Lee", "Sean Marzug-McCarthy", "Bryan Reid", "Adrian Sandy", "Jane Sternbach", "Chloe Stinetorf", "Sandip Trivedi", "Joseph Waine", "Luke Walker"]
 puts "How many students would you like in each group?"
   size = gets.chomp.to_i
-  groups = size.each_slice(size).to_a
+  groups = students.each_slice(size).to_a
 
+groups.each { |i| i.length < size ? groups[-1].concat(groups[-2]) : groups}
