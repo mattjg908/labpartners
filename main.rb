22d4f2f3aff4b19fbe3c95467849068d5a9a5da3
Matt Galvin <matthewgalvin908@gmail.com>
2013-01-30 16:21:23 -0500

    retrieve specified group size from user

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
 students = ["Stephen Bae", "Ralph Bernardo", "Nick Blanchet", "Ben Clayman", "Tony Contreras", "Alex Fogg", "Shefali Friesen", "Matthew Galvin", "Eric Holland", "Nicky Hughes", "Ron Jones", "Jeffrey Katz", "Ryan Lee", "Sean Marzug-McCarthy", "Bryan Reid", "Adrian Sandy", "Jane Sternbach", "Chloe Stinetorf", "Sandip Trivedi", "Joseph Waine", "Luke Walker"]
 puts "How many students would you like in each group?"
   size = gets.chomp.to_i
+  groups = size.each_slice(size).to_a
+
