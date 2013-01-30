eb61640c50bf000459d63627dae0b8621ef8c621
Matt Galvin <matthewgalvin908@gmail.com>
2013-01-30 16:11:36 -0500

    add user group size question

--- main.rb
+++ main.rb
@@ -1,1 +1,3 @@
 students = ["Stephen Bae", "Ralph Bernardo", "Nick Blanchet", "Ben Clayman", "Tony Contreras", "Alex Fogg", "Shefali Friesen", "Matthew Galvin", "Eric Holland", "Nicky Hughes", "Ron Jones", "Jeffrey Katz", "Ryan Lee", "Sean Marzug-McCarthy", "Bryan Reid", "Adrian Sandy", "Jane Sternbach", "Chloe Stinetorf", "Sandip Trivedi", "Joseph Waine", "Luke Walker"]
+puts "How many students would you like in each group?"
+  size = gets.chomp.to_i
