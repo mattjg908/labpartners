fef1c1e83986a665054f42fe1fad9202679f5320
Ron Jones <rrrron@gmail.com>
2013-01-30 17:01:35 -0500

    What I have so far

--- main.rb
+++ main.rb
@@ -1,6 +1,13 @@
+require "pry-debugger"
+
 students = ["Stephen Bae", "Ralph Bernardo", "Nick Blanchet", "Ben Clayman", "Tony Contreras", "Alex Fogg", "Shefali Friesen", "Matthew Galvin", "Eric Holland", "Nicky Hughes", "Ron Jones", "Jeffrey Katz", "Ryan Lee", "Sean Marzug-McCarthy", "Bryan Reid", "Adrian Sandy", "Jane Sternbach", "Chloe Stinetorf", "Sandip Trivedi", "Joseph Waine", "Luke Walker"]
 puts "How many students would you like in each group?"
-  size = gets.chomp.to_i
-  groups = students.each_slice(size).to_a
 
-groups.each { |i| i.length < size ? groups[-1].concat(groups[-2]) : groups}
+size = gets.chomp.to_i
+
+groups = students.each_slice(size).to_a
+binding.pry
+groups
+# groups.each { |i| i.length < size ? (i[-1] + i[-2]) : (puts groups) }
+
+groups.any? < size
